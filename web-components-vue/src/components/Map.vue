7b94b260d82bd9542ee2fba35db395754356d406
Omar Kawach <OKawach@esri.com>
2024-03-12 11:10:08 -0700

    add custom element

--- web-components-vue/src/components/Map.vue
+++ web-components-vue/src/components/Map.vue
@@ -1,27 +1,33 @@
 <template>
-  <arcgis-map    
-    item-id="866f3ab5307240d49f62ba95c2f452ca"
-    v-on:arcgisViewReadyChange="onReady"
-  >
-  <arcgis-expand mode="floating" position="top-right">
-    <arcgis-search></arcgis-search>
-  </arcgis-expand>
-
+  <arcgis-map item-id="866f3ab5307240d49f62ba95c2f452ca" v-on:arcgisViewReadyChange="onReady">
+    <arcgis-expand mode="floating" position="top-right">
+      <arcgis-search></arcgis-search>
+    </arcgis-expand>
+    
+    <arcgis-placement position="bottom-right">
+      <calcite-notice open icon="code" closable>
+        <div slot="title">View the source code for this application</div>
+        <calcite-link slot="link"
+          href="https://github.com/kellyhutchins/DeveloperSummitDemos2024/tree/main" target="_blank">
+          GitHub
+        </calcite-link>
+      </calcite-notice>
+    </arcgis-placement>
   </arcgis-map>
 </template>
 
-<script setup >
-
+<script setup>
 const emit = defineEmits(["mapReady"]);
 
 function onReady(event) {
   const { map } = event.target;
-  emit('mapReady',map);
-  
+  emit('mapReady', map);
+
 }
 </script>
+
 <style>
-arcgis-map{
-  height:100%;
+arcgis-map {
+  height: 100%;
 }
 </style>
